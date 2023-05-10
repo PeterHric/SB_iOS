103fb5c3879e27d0b21ce70701b0d59b6511cc9d
Peter HRIC <hric.peter@gmail.com>
2023-05-10 23:37:50 +0200

    Adding main page and Web View & rest of BL

--- WebView.xaml.cs
+++ WebView.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Reflection.Emit;
 
-using Xamarin.Forms;
+using Xamarin.Essentials;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using System;
+using System.Net.NetworkInformation;
 
-namespace SB_iOS
+namespace MyWebApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WebView : ContentPage
@@ -15,6 +15,59 @@ namespace SB_iOS
         public WebView()
         {
             InitializeComponent();
+
+            // Check internet availability
+            if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                // Check that our web target is accessible
+                var sbUcenieUrl = "https://ucenie.smartbooks.sk";
+                var webUri = new System.Uri(sbUcenieUrl);
+
+                if (PingHost(sbUcenieUrl))
+                {
+                    // Show the BL content in the web view
+                    TheWebView.Source = sbUcenieUrl;
+                }
+                else
+                {
+                    // Web site is not accessible
+                    TheWebView.Source = "<html><body><h1>Server SmartBooks je nedostupný.</h1></body></html>";
+                }
+            }
+            else
+            {
+                // Display no internet connection error
+                var errorMessage = "<html><body><h1>Chyba: Nie je k dispozícii pripojenie k internetu.</h1></body></html>";
+                var errorUrl = "https://www.google.com/";
+                TheWebView.Source = errorMessage;
+                NavigationPage.SetTitleView(this, new Label { Text = "Chyba: Nie je k dispozícii pripojenie k internetu.", TextColor = Color.White });
+                ToolbarItems.Add(new ToolbarItem("Info", "", async () => await Browser.OpenAsync(errorUrl, BrowserLaunchMode.SystemPreferred)));
+            }
+        }
+
+        private bool PingHost(string nameOrAddress, int timeout = 1000, int count = 1)
+        {
+            using (var pinger = new Ping())
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    try
+                    {
+                        var reply = pinger.Send(nameOrAddress, timeout);
+                        if (reply.Status == IPStatus.Success)
+                        {
+                            return true;
+                        }
+                    }
+                    catch (PingException)
+                    {
+                        // Failed to ping the URL, or internet connection not available
+                        return false;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
